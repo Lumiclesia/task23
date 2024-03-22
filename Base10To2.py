4f4e83a11b0fdf7c73612c8e386a949385e7d479
Lumiclesia <kossi.g@outlook.com>
2024-03-22 23:44:37 +0000

    Version 1.2 Convert from Base2 into Base10

--- Base10To2.py
+++ Base10To2.py
@@ -16,9 +16,7 @@ def changeAds(base10):
     for digit in base2:
         base2ok.insert(0,digit)
         #print(f"{digit}, ,{base2ok}")
-
     # print(base2ok)
-
     return base2ok
 
 def Negate(base2):
@@ -30,11 +28,34 @@ def Negate(base2):
             negate2.insert(i,'0')
     return negate2
 
-n = input("Enter a number : ")
-base2 = changeAds(int(n))
-negate2 = Negate(base2)
-print(f"Here is {n} convertion int Base 2: {base2}")
-print(f"The negate binary of {base2} is : {negate2}")
+def Base2to10(base2):
+    base10 = 0
+    power = len(base2)
+    indice = power-1
+    for i in range(0, power):
+        base10 += int(base2[i]) * pow(2,indice-i)
+        # print(power, i, base2[i], base10, pow(2,power+1-i) )
+    return base10
+
+while True:
+    n = input("Enter a number : ")
+    try:
+        if int(n) >=0:
+            base2 = changeAds(int(n))
+            negate2 = Negate(base2)
+            base10 = Base2to10(negate2)
+            print(f"Here is {n} convertion into Base 2 : {base2}")
+            print(f"The negate binary of {base2} is : {negate2}")
+            print(f"Here is {negate2} convertion into base 10 :{base10}")
+            break
+        else:
+            print("Incorrect entry")
+            continue
+    except ValueError:
+        print("Incorrect entry. You must enter a number !")
+        continue
+
+
 
 
 # Negate 
